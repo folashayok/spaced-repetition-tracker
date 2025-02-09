05d94e5ee5dfd73a0a71c51e22c67e23dae10a31
folashayok <folashayokehinde11@gmail.com>
2025-02-09 18:19:06 -0500

    installed shadCN

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eMap/UV95x2bkwh68yj2QFqCT1kAM0e1p2LROtxL++8=\"\n}"
